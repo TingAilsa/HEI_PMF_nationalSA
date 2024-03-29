b124af2a1446a2dcdf342975862a4e889df86a1b
TingAilsa <ailsa2be@gmail.com>
2024-03-29 00:01:48 -0400

    move multiplot into function

--- 0. US_SA_functions.R
+++ 0. US_SA_functions.R
@@ -167,6 +167,45 @@ panel.scatter <- function(x, y, ...){
                          alpha.f=point_alpha))
 }
 
+
+#### arrange the position of multiple plots ####
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+
 ##########################################################################################
 ############ 2. DATA PROCESS ############ 
 ##########################################################################################
@@ -1251,18 +1290,4 @@ species_col_reorder = function(species_df) {
   
   return(species_df)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
